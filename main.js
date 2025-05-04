f6a8921748a80039f7b1ded78c737406c1025de3
hmn <hmn@gmail.com>
2025-05-04 21:51:50 +0100

    add logs

--- main.js
+++ main.js
@@ -2,22 +2,29 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async function (req, res) {
+export default async function ({ req, res, log }) {
   try {
-    const { firstName, lastName , phoneNumber ,  email, message } = JSON.parse(req.body);
+    const { firstName, lastName, phoneNumber, email, message } = JSON.parse(
+      req.body
+    );
 
     const { data, error } = await resend.emails.send({
-        from: 'onboarding@resend.dev',
-        to: 'issamedhamani@outlook.com',
-        subject: 'Hello Portfolio',
-        html: `
+      from: "onboarding@resend.dev",
+      to: "issamedhamani@outlook.com",
+      subject: "Hello Portfolio",
+      html: `
         <p><strong>Name:</strong> ${firstName}${lastName}</p>
         <p><strong>Phone:</strong> ${phoneNumber}</p>
         <p><strong>Email:</strong> ${email}</p>
         <p><strong>Message:</strong> ${message}</p>
       `,
-      })
+    });
 
+    log("Email sent:", data);
+    log("headers is:", JSON.stringify(req.headers));
+    log("Request from host:", req.host);
+
+    return res.json({ success: true, data });
   } catch (error) {
     console.error("Function Error:", err);
     res.status(500).json({ success: false, error: err.message });
